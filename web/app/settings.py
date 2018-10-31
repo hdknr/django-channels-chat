6e0f1fdc050cbd4b701b9002e3a63d1000ed4d02
hdknr <gmail@hdknr.com>
2018-10-31 21:14:02 +0900

    CHANEL_LAYTER for redis-server

--- web/app/settings.py
+++ web/app/settings.py
@@ -124,3 +124,11 @@ INSTALLED_APPS += [
     'chat',
 ]
 ASGI_APPLICATION = 'app.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
